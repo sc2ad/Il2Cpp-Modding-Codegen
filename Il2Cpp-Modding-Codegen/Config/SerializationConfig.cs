21b6d360b5f558fdc88dcc761d9f34165c3dc2ad
Adam ? <adamznow@gmail.com>
2020-05-18 16:19:35 -0700

    Add config options (not yet implemented)
    Need to handle these config options correctly

--- Il2Cpp-Modding-Codegen/Config/SerializationConfig.cs
+++ Il2Cpp-Modding-Codegen/Config/SerializationConfig.cs
@@ -9,5 +9,54 @@ namespace Il2Cpp_Modding_Codegen.Config
         public string OutputDirectory { get; set; }
         public string OutputHeaderDirectory { get; set; }
         public string OutputSourceDirectory { get; set; }
+
+        /// <summary>
+        /// How to output the created methods
+        /// </summary>
+        public OutputStyle OutputStyle { get; set; }
+
+        /// <summary>
+        /// How to handle unresolved type exceptions
+        /// </summary>
+        public UnresolvedTypeExceptionHandling UnresolvedTypeExceptionHandling { get; set; }
+
+        /// <summary>
+        /// Types blacklisted are explicitly not converted, even if it causes unresolved type exceptions in other types
+        /// </summary>
+        public List<string> BlacklistTypes { get; set; }
+
+        /// <summary>
+        /// Types whitelisted are explicitly converted, even if some have unresolved type exceptions
+        /// </summary>
+        public List<string> WhitelistTypes { get; set; }
+
+        /// <summary>
+        /// Will attempt to resolve all type exceptions (ignores blacklists and whitelists to look through full context)
+        /// </summary>
+        public bool EnsureReferences { get; set; }
+
+        /// <summary>
+        /// How to handle generics
+        /// </summary>
+        public GenericHandling GenericHandling { get; set; }
+    }
+
+    public enum OutputStyle
+    {
+        CrashUnless,
+        Normal
+    }
+
+    public enum UnresolvedTypeExceptionHandling
+    {
+        DisplayInFile,
+        Continue,
+        Skip
+    }
+
+    public enum GenericHandling
+    {
+        Do,
+        Skip
     }
 }
