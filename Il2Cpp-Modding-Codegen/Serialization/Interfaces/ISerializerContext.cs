b40a02a6f3a33a1050aa91b357e682d08b306b72
Adam ? <adamznow@gmail.com>
2020-05-27 19:24:11 -0700

    Write interfaces better

--- Il2Cpp-Modding-Codegen/Serialization/Interfaces/ISerializerContext.cs
+++ Il2Cpp-Modding-Codegen/Serialization/Interfaces/ISerializerContext.cs
@@ -11,7 +11,7 @@ namespace Il2Cpp_Modding_Codegen.Serialization.Interfaces
         string TypeName { get; }
         string QualifiedTypeName { get; }
 
-        string GetNameFromReference(TypeDefinition def, ForceAsType force = ForceAsType.None, bool qualified = true);
+        string GetNameFromReference(TypeDefinition def, ForceAsType force = ForceAsType.None, bool qualified = true, bool genericParams = true);
     }
 
     public enum ForceAsType
