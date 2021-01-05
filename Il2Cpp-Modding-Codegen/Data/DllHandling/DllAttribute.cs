41e2162c11dda6baf437ccf91e77095e1b8d09b0
Adam ? <adamznow@gmail.com>
2021-01-05 00:41:41 -0800

    Ignore TokenAttributes

--- Il2Cpp-Modding-Codegen/Data/DllHandling/DllAttribute.cs
+++ Il2Cpp-Modding-Codegen/Data/DllHandling/DllAttribute.cs
@@ -7,6 +7,7 @@ namespace Il2CppModdingCodegen.Data.DllHandling
     {
         // Name is the name field of the attribute
         public string Name { get; } = "";
+
         public int RVA { get; } = -1;
         public int Offset { get; } = -1;
         public int VA { get; } = -1;
@@ -16,6 +17,7 @@ namespace Il2CppModdingCodegen.Data.DllHandling
             // These parameters are unknown for attributes besides the attribute that has all three in its constructor
             if (attribute.Fields.Count == 3)
                 foreach (var f in attribute.Fields)
+                {
                     if (f.Name == "Name")
                         Name = (string)f.Argument.Value;
                     else if (f.Name == "RVA" || f.Name == "Offset" || f.Name == "VA")
@@ -25,6 +27,10 @@ namespace Il2CppModdingCodegen.Data.DllHandling
                         else if (f.Name == "Offset") Offset = val;
                         else if (f.Name == "VA") VA = val;
                     }
+                }
+            else if (attribute.AttributeType.FullName != "Il2CppDummyDll.TokenAttribute")
+                // Ignore TokenAttribute
+                Name = attribute.AttributeType.Name;
         }
 
         public override string ToString() => $"[{Name}] // Offset: 0x{Offset:X}";
